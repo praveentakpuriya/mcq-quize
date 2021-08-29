4fa940fd522782ae2c714a51f38927987af57bf5
c-sed <github@estah.org>
2021-08-30 00:04:41 +0530

    updated by praveen

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: "Q",
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Modern Language",
+      "Hyper Tool Markup Language",
+      "Hyper Test Multiple Language",
+      "Hyper Text Markup Language"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Cascading Style Size",
+      "Colorful Style Sheet",
+      "Common Sheet Style",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "Which of the following is Web Browser?",
+    answer: "Google",
+    options: [
+      "Facebook",
+      "Google",
+      "Whatsapp",
+      "Slack"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Query Language",
+      "Stylesheet Query Language",
+      "Structured Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "Which is Version Control Software?",
+    answer: "Git",
+    options: [
+      "Git",
+      "AWS",
+      "Container",
+      "JavaScript"
+    ]
+  },
+ 
+  {
+    numb: "Q",
+    question: "What does AWS stand for?",
+    answer: "Amazon Web Services",
+    options: [
+      "Amazon Web Services",
+      "Apple Web Server",
+      "Amazon Work Services",
+      "Amazon Web Server"
+    ]
+  },
+    {
+      numb: "Q",
+      question: "Which is Cloud Providing Platform?",
+      answer: "AWS",
+      options: [
+        "Docker",
+        "Kubernetes",
+        "GSP",
+        "AWS"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "Extension of JavaScript?",
+      answer: ".js",
+      options: [
+        ".jt",
+        ".js",
+        ".j",
+        ".java"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "Extension of Python is?",
+      answer: ".py",
+      options: [
+        ".python",
+        ".py",
+        ".p",
+        ".pyt"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "AWS is Cloud Servies Provided By?",
+      answer: "Amazon",
+      options: [
+        "Google",
+        "Apple",
+        "Amazon",
+        "Microsoft"
+      ]
+    },
+];
